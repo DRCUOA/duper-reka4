abe26104089efabb2f78e1c71631999eb694f0ae
Richard Clark <dcla839@aucklanduni.ac.nz>
2022-09-12 17:52:58 +1200

    setup

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import { Player } from './player.js';
 import { InputHandler } from './input.js';  
 import { Background } from "./background.js";
+import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from "./enemies.js"
 
 window.addEventListener('load', function () {
   const canvas = document.querySelector('#canvas1');
   const ctx = canvas.getContext('2d');
-  canvas.width = 500;
+  canvas.width = 1500;
   canvas.height = 500;
 
   class Game {
@@ -18,14 +19,37 @@ window.addEventListener('load', function () {
       this.background = new Background(this);
       this.player = new Player(this);
       this.input = new InputHandler();
+      this.enemies = [];
+      this.enemyTimer = 0;
+      this.enemyInterval = 1000;  // speed of adding enemies
     }
     update(deltaTime) {
       this.background.update();
       this.player.update(this.input.keys, deltaTime);
+      // handleEnemies
+      if(this.enemyTimer > this.enemyInterval) {
+        this.addEnemy();
+        this.enemyTimer = 0;
+      } else {
+        this.enemyTimer += deltaTime;
+      }
+      this.enemies.forEach(enemy => {
+        enemy.update(deltaTime);
+        if( enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy), 1);
+      })
+
     }
     draw(context) {
       this.background.draw(context);
-      this.player.draw(context)
+      this.player.draw(context);
+      this.enemies.forEach(enemy => {
+        enemy.draw(context);
+      })
+
+    }
+    addEnemy(){
+      this.enemies.push(new FlyingEnemy(this));
+      console.log(this.enemies);
     }
   }
 
