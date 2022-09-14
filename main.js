02f883262ebbf963018107c80da986429fcc408a
Richard Clark <dcla839@aucklanduni.ac.nz>
2022-09-14 17:30:30 +1200

    ed refact

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import { Player } from './player.js';
 import { InputHandler } from './input.js';  
 import { Background } from "./background.js";
 import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from "./enemies.js"
+import { UI } from "./UI.js";
 
 window.addEventListener('load', function () {
   const canvas = document.querySelector('#canvas1');
@@ -18,10 +19,17 @@ window.addEventListener('load', function () {
       this.maxSpeed = 4;
       this.background = new Background(this);
       this.player = new Player(this);
-      this.input = new InputHandler();
+      this.input = new InputHandler(this);
+      this.UI = new UI(this);
       this.enemies = [];
       this.enemyTimer = 0;
       this.enemyInterval = 1000;  // speed of adding enemies
+      this.debug = true;
+      this.score = 0;
+      this.fontColour = 'red';
+      this.player.currentState = this.player.states[0];
+      this.player.currentState.enter();
+
     }
     update(deltaTime) {
       this.background.update();
@@ -45,19 +53,17 @@ window.addEventListener('load', function () {
       this.enemies.forEach(enemy => {
         enemy.draw(context);
       })
+      this.UI.draw(context);
 
     }
     addEnemy(){
       if(this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
       else if(this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
       this.enemies.push(new FlyingEnemy(this));
-      console.log(this.enemies);
-
     }
   }
 
   const game = new Game(canvas.width, canvas.height);
-  console.log(game);
   let lastTime = 0;
 
   function animate(timeStamp){
